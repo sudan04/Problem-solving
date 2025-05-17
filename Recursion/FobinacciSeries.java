496e27b213bfa149a897b19363bcea473fd6ae26
sudan04 <bhusalsudan79@gmail.com>
2025-05-17 17:06:21 +0545

    Fibonacci series with recursion

--- Recursion/FobinacciSeries.java
+++ Recursion/FobinacciSeries.java
@@ -2,6 +2,17 @@ package Recursion;
 
 public class FobinacciSeries {
     public static void main(String[] args) {
-        
+        int n=10;
+        for(int i=0; i<=n; i++){
+            System.out.print(fib(i)+" ");
+        }
+        System.out.println();
+    }
+    
+
+    public static int fib(int n){
+        if(n ==0 || n == 1) return n;
+
+        return fib(n-1)+fib(n-2);
     }
 }
