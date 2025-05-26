d5094819cadfa133f2672837f6aa9378b4df5434
sudan04 <bhusalsudan79@gmail.com>
2025-05-26 18:29:16 +0545

    Modofied

--- Recursion/SumOfDigits.java
+++ Recursion/SumOfDigits.java
@@ -2,7 +2,9 @@ package Recursion;
 
 public class SumOfDigits {
     public static void main(String[] args) {
-        System.out.println(sumOfDigits(6786));
+        long n= 6786;
+        System.out.println("Sum of digits in "+ n+": "+sumOfDigits(n));
+        System.out.println("Product of digits in "+ n+": "+productOfDigits(n));
     }
 
     // finds the sum of digits of a number( n=1234, return 1+2+3+4)
@@ -10,5 +12,12 @@ public class SumOfDigits {
         if(n==0) return 0;
 
         return n%10 + sumOfDigits(n/10);
+    }
+
+    // finds the product of digits of a number( n=1234, return 1*2*3*4)
+    static long productOfDigits(long n){
+        if(n==0) return 1;
+
+        return n%10 * productOfDigits(n/10);
     }
 }
