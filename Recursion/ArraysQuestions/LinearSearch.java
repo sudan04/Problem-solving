12c6255534a9e5f2575f4604139d7f4191dc8e89
sudan04 <bhusalsudan79@gmail.com>
2025-05-30 21:25:00 +0545

    Linear search with recursion

--- Recursion/ArraysQuestions/LinearSearch.java
+++ Recursion/ArraysQuestions/LinearSearch.java
@@ -1,10 +1,14 @@
 package Recursion.ArraysQuestions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LinearSearch {
     public static void main(String[] args) {
-        int arr[]= {3,4,6,2,78,56,324,23};
+        int arr[]= {3,4,6,2,78,56,324,23,6,34,6};
 
-        System.out.println(find(arr, 56, 0));
+        // System.out.println(find(arr, 56, 0));
+        System.out.println(findNums(arr,6 , 0, new ArrayList<>()));
     }
 
     static int find(int[] arr, int target, int l){
@@ -13,5 +17,19 @@ public class LinearSearch {
         if(arr[l]== target) return l;
 
         return find(arr, target, l+1);
+    }
+
+
+
+    // multiple occurrence search: return a List
+    static List<Integer> findNums(int arr[], int target, int l, List<Integer> list){
+        if(l==arr.length) return list;
+
+        if(target== arr[l]){
+            list.add(l);
+            return findNums(arr, target, l+1, list);
+        }
+
+        return findNums(arr, target, l+1, list);
     }
 }
