b31ef7601286cbe0e9a56becfdbe05dd24307ed6
sudan04 <bhusalsudan79@gmail.com>
2025-08-17 21:39:08 +0545

    Skip character modified

--- Recursion/strings/SkipCharacter.java
+++ Recursion/strings/SkipCharacter.java
@@ -8,12 +8,19 @@ public class SkipCharacter{
     public static void main(String[] args){
         String str= "bcadabm";
 
+        // System.out.println("Before: "+ str);
+        // skipCharacter(str, 'a');
+
+        // System.out.println("After: "+ ans);
+
         System.out.println("Before: "+ str);
         skipCharacter(str, 'a');
 
-        System.out.println("After: "+ ans);
+        System.out.println("After: "+ skipChar(str, 'b'));
     }
 
+
+    // void function 
     public static void skipCharacter(String str, char chr){
         if(str.length()<=0)
             return;
@@ -25,5 +32,20 @@ public class SkipCharacter{
             skipCharacter(str.substring(1,str.length()), chr);
 
         }
+    }
+
+
+    // returns string
+    public static String skipChar(String str, char chr){
+        if(str.isEmpty())
+            return "";
+        
+        if(str.charAt(0)==chr){
+            return skipChar(str.substring(1), chr);
+        }
+            ans.append(str.charAt(0));
+            return str.charAt(0) + skipChar(str.substring(1), chr);
+
+        
     }
 }
